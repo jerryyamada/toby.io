97a643103fd76bb0cd17d3d843087786f5ca2fff
Jerry Yamada <jerry_yamada@yahoo.com>
2021-03-27 23:12:58 -0700

    x

---
+++ js/onecall.js
@@ -0,0 +1,55 @@
+let appid = '7902395e981f096d6e1c661d78975faa';
+let lat='37.3382';
+let lon='-121.8863';
+
+// const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=37.3382&lon=-121.8863&units=imperial&exclude=minutely,hourly&appid=7902395e981f096d6e1c661d78975faa`;
+// const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${appid}`;
+ const apiURL = 'js/onecall.json'
+
+fetch(apiURL)
+  .then(function (response) {
+    return response.json()
+  })
+
+  .then(function (dataObject) {
+    console.log(dataObject)
+
+    for (let i = 0; i < dataObject.daily.length; i++) {
+
+      var date = new Date(dataObject.daily[i].dt * 1000);
+      // console.log(dataObject.daily[i].dt);
+      console.log(date);
+      console.log(dataObject.current.temp);
+
+      /*
+      if (dataObject.list[i].dt_txt.includes('18:00:00')) {
+        let card = document.createElement('section')
+        var date = new Date(dataObject.list[i].dt_txt)
+        const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        document.querySelector('div.cards').appendChild(card)
+
+        let p1 = document.createElement('p')
+        p1.textContent = dayOfWeek[date.getDay()]
+        card.appendChild(p1)
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + dataObject.list[i].weather[0].icon  + '.png'
+        //p2.textContent = `<img src="${imagesrc}" alt="${dataObject.list[i].weather[0].description}" id="icon">`;
+
+      let img = document.createElement('img')
+      img.setAttribute('src', imagesrc)
+      img.alt = `$dataObject.list[i].weather[0].description`
+      img.setAttribute('id', 'icon')
+      img.style.width = '60px'
+      card.appendChild(img)
+
+        let p3 = document.createElement('p')
+        p3.textContent = dataObject.list[i].main.temp
+        card.appendChild(p3)
+
+      }
+      */
+    }
+
+  })
+
+
