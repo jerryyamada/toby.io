3e05a377f3ae91a31d37b7e966ed135376c309c3
Jerry Yamada <jerry_yamada@yahoo.com>
2021-03-28 20:03:32 -0700

    x

--- js/onecall.js
+++ js/onecall.js
@@ -1,10 +1,10 @@
-let appid = '7902395e981f096d6e1c661d78975faa';
-let lat='37.3382';
-let lon='-121.8863';
+let appid = '7902395e981f096d6e1c661d78975faa'
+// let lat = '37.3382'
+// let lon = '-121.8863'
 
 // const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=37.3382&lon=-121.8863&units=imperial&exclude=minutely,hourly&appid=7902395e981f096d6e1c661d78975faa`;
 // const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${appid}`;
- const apiURL = 'js/onecall.json'
+const apiURL = 'js/onecall.json'
 
 fetch(apiURL)
   .then(function (response) {
@@ -13,15 +13,17 @@ fetch(apiURL)
 
   .then(function (dataObject) {
     console.log(dataObject)
+    if (dataObject.daily.length > 1) {
+      for (let i = 0; i < 3; i++) {
+        var date = new Date(dataObject.daily[i].dt * 1000)
+        // console.log(dataObject.daily[i].dt);
+        console.log(date)
+        var day = date.getDay()
+        const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        console.log(dayOfWeek[day])
+        console.log(dataObject.current.temp)
 
-    for (let i = 0; i < dataObject.daily.length; i++) {
-
-      var date = new Date(dataObject.daily[i].dt * 1000);
-      // console.log(dataObject.daily[i].dt);
-      console.log(date);
-      console.log(dataObject.current.temp);
-
-      /*
+        /*
       if (dataObject.list[i].dt_txt.includes('18:00:00')) {
         let card = document.createElement('section')
         var date = new Date(dataObject.list[i].dt_txt)
@@ -48,8 +50,12 @@ fetch(apiURL)
 
       }
       */
+      }
     }
 
+    if (dataObject.hasOwnProperty('alerts')) {
+      console.log('alert exists')
+    } else {
+      console.log('alert does not exist')
+    }
   })
-
-
